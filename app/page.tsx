83878bc18ca6e4bac525324ea415cf63a6e9439b
DjangoBona <andohenrirazafinatonadro@gmail.com>
2026-03-02 12:42:00 +0300

    add Hero and book list section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import BookCard from "@/components/BookCard";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+
 const Page = () => {
   return (
-    <div className='text-underline text-2xl'>Bookified</div>
-  )
-}
+    <main className="wrapper container pt-24 pb-12">
+      <HeroSection />
 
-export default Page
+      <div className='library-books-grid'>
+        {sampleBooks.map((book) => (
+          <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Page;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://covers.openlibrary.org/**"),
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
